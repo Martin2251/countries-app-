4910cc06fcaba73cfc28ff0b61f77f6de796ead1
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-03-03 17:32:25 +0000

    so far

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import {useState, useEffect} from "react"
 
 const Countries = () => {
+    const [countries,setCountries] = useState ([])
+
+    useEffect (() => {
+        const getCountries = async () => {
+            try {
+                const res = await fetch("https://restcountries.com/v3.1/all")
+                const data = await res.json()
+                setCountries(data)
+            } catch (error) {
+                console.error(error)
+            }
+        }
+        getCountries()
+    }, [])
   return (
-    <div>Countries</div>
+   <>
+ {!countries ? <h1 className='text-gray-900 font-bold uppercase tracking-wide flex-center justify-center text-center h-screen text-4xl dark:text-white'> Loading</h1>: <section>
+    
+    
+    </section>}
+   </>
   )
 }
 
