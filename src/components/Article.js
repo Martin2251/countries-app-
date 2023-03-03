f5b5e5aa5bc6d964a68f2c80dbc05701fd9f8d1a
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-03-03 17:51:36 +0000

    key name wrong

---
+++ src/components/Article.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Article = ({flags,name,population,region,subregion}) => {
+  return (
+   <>
+   <article>
+    <img src={flags.svg} alt=""  />
+    <h2>{name.common}</h2>
+    <ul>
+        <li>Population: {population}</li>
+        <li>Region : {region}</li>
+        <li>Subregion: {subregion}</li>
+    </ul>
+   </article>
+   
+   </>
+  )
+}
+
+export default Article
--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,29 +1,38 @@
-import React from 'react'
-import {useState, useEffect} from "react"
+import React from "react";
+import { useState, useEffect } from "react";
+import Article from "./Article";
 
 const Countries = () => {
-    const [countries,setCountries] = useState ([])
+  const [countries, setCountries] = useState([]);
 
-    useEffect (() => {
-        const getCountries = async () => {
-            try {
-                const res = await fetch("https://restcountries.com/v3.1/all")
-                const data = await res.json()
-                setCountries(data)
-            } catch (error) {
-                console.error(error)
-            }
-        }
-        getCountries()
-    }, [])
+  useEffect(() => {
+    const getCountries = async () => {
+      try {
+        const res = await fetch("https://restcountries.com/v3.1/all");
+        const data = await res.json();
+        setCountries(data.slice(0,10));
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getCountries();
+  }, []);
   return (
-   <>
- {!countries ? <h1 className='text-gray-900 font-bold uppercase tracking-wide flex-center justify-center text-center h-screen text-4xl dark:text-white'> Loading</h1>: <section>
-    
-    
-    </section>}
-   </>
-  )
-}
+    <>
+      {!countries ? (
+        <h1 className="text-gray-900 font-bold uppercase tracking-wide flex-center justify-center text-center h-screen text-4xl dark:text-white">
+          {" "}
+          Loading....
+        </h1>
+      ) : (
+        <section>
+          {countries.map((country) => (
+            <Article key={countries.name} {...country}></Article>
+          ))}
+        </section>
+      )}
+    </>
+  );
+};
 
-export default Countries
+export default Countries;
