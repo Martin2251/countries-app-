3b04801aecae5d40bc27de28d9e87f69c7329e79
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-03-04 10:08:10 +0000

    border logic working

--- src/components/SingleCountry.js
+++ src/components/SingleCountry.js
@@ -1,56 +1,63 @@
-import React from 'react'
-import {useState, useEffect} from "react"
-import { useParams } from 'react-router-dom';
-
+import React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 const SingleCountry = () => {
-    const [country, setCountry] = useState([]);
-    const {name} = useParams();
+  const [country, setCountry] = useState([]);
+  const { name } = useParams();
 
-
-    useEffect (() => {
-        const getSingleCountry = async () => {
-            try {
-                const res = await fetch (`https://restcountries.com/v3.1/name/${name}`)
-                const data = await res.json()
-                setCountry(data)
-                
-            } catch (error) {
-                console.error(error)
-            }
-        }
-        getSingleCountry()
-
-    }, [name])
+  useEffect(() => {
+    const getSingleCountry = async () => {
+      try {
+        const res = await fetch(`https://restcountries.com/v3.1/name/${name}`);
+        const data = await res.json();
+        setCountry(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getSingleCountry();
+  }, [name]);
   return (
-  <>
-  <section className='p-8 md:py-0 max-w-7xl mx-auto'>
-   {country.map((item) => (
-    <div key={item.population} className="grid grid cols-1 gap-8 md:grid-cols-2 md:place-items-center md:h-screen">
-        <article>
-            <img src={item.flags.svg} alt={item.name}  />
-        </article>
-        <article>
-            <h1 className='font-bold text-gray-900 text-4xl lg:text-6xl'>{item.name.official}</h1>
-            <ul>
-                <li>Capital:</li>
-                <li>Population:</li>
-                <li>Region:</li>
-                <li>Subregion:</li>
+    <>
+      <section className="p-8 md:py-0 max-w-7xl mx-auto">
+        {country.map((item) => (
+          <div
+            key={item.population}
+            className="grid grid cols-1 gap-8 md:grid-cols-2 md:place-items-center md:h-screen"
+          >
+            <article>
+              <img src={item.flags.svg} alt={item.name} />
+            </article>
+            <article>
+              <h1 className="font-bold text-gray-900 text-4xl lg:text-6xl">
+                {item.name.official}
+              </h1>
+              <ul>
+                <li>Capital: {item.capital[0]}</li>
+                <li>Population:{item.population.toLocaleString()}</li>
+                <li>Region:{item.region}</li>
+                <li>Subregion:{item.subregion}</li>
                 <li>Currency:</li>
-                
-            </ul>
-            <h3>Borders</h3>
-            <ul>
-                {}
-            </ul>
-        </article>
-    </div>
-   ))}
-  </section>
-  
-  </>
-  )
-}
+              </ul>
+           {item.borders && 
+           <>
+           <h3>Borders</h3>
+           <ul>
+            {item.borders.map((border,index) =>(
+                <li key={index}>{border}</li>
+            ))}
+           </ul>
+           </>
+           }
 
-export default SingleCountry
+
+            </article>
+          </div>
+        ))}
+      </section>
+    </>
+  );
+};
+
+export default SingleCountry;
