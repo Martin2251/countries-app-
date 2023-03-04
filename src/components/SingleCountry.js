b8ae4c7d63567f434b175d6a38bc9c9916301213
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-03-04 09:51:13 +0000

    so far

--- src/components/SingleCountry.js
+++ src/components/SingleCountry.js
@@ -24,14 +24,26 @@ const SingleCountry = () => {
     }, [name])
   return (
   <>
-  <section>
+  <section className='p-8 md:py-0 max-w-7xl mx-auto'>
    {country.map((item) => (
-    <div key={item.population}>
+    <div key={item.population} className="grid grid cols-1 gap-8 md:grid-cols-2 md:place-items-center md:h-screen">
         <article>
             <img src={item.flags.svg} alt={item.name}  />
         </article>
         <article>
-            <h1>{item.name.official}</h1>
+            <h1 className='font-bold text-gray-900 text-4xl lg:text-6xl'>{item.name.official}</h1>
+            <ul>
+                <li>Capital:</li>
+                <li>Population:</li>
+                <li>Region:</li>
+                <li>Subregion:</li>
+                <li>Currency:</li>
+                
+            </ul>
+            <h3>Borders</h3>
+            <ul>
+                {}
+            </ul>
         </article>
     </div>
    ))}
