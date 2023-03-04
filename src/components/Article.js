a4f4c846dc2288447df732480eb18fdecd6ff031
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-03-04 08:54:55 +0000

    added tailwind classes

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,14 +3,16 @@ import React from 'react'
 const Article = ({flags,name,population,region,subregion}) => {
   return (
    <>
-   <article>
-    <img src={flags.svg} alt=""  />
+   <article className='bg-white rounded-lg shadow'>
+    <img src={flags.svg} alt="" className='md:h-72 w-full object-cover' />
+    <div className='p-4'>
     <h2 className='font-bold text-lg text-gray-900 mb-2'>{name.common}</h2>
     <ul>
         <li>Population: {population.toLocaleString()}</li>
         <li>Region : {region}</li>
         <li>Subregion: {subregion}</li>
     </ul>
+    </div>
    </article>
    
    </>
--- src/components/Countries.js
+++ src/components/Countries.js
@@ -26,9 +26,11 @@ const Countries = () => {
         </h1>
       ) : (
         <section>
+            <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
           {countries.map((country) => (
             <Article key={countries.name} {...country}></Article>
           ))}
+          </div>
         </section>
       )}
     </>
