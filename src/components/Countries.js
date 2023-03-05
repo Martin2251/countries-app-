a1df1e7907d0be71bf0867ba5c11a327764cf854
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-03-05 11:01:22 +0000

    search text

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -39,9 +39,20 @@ const Countries = () => {
     getCountries();
   }, []);
 
-  const handleSearchCountry = (e) => {
-    e.preventDefault()
-    SearchCountry()
+  async function searchCountry(){
+    try {
+        const res = await fetch (`https://restcountries.com/v3.1/name/${searchText}`)
+        const data = await res.json()
+        setCountries(data)
+    } catch (error) {
+        console.error(error)
+        
+    }
+  }
+
+  function handleSearchCountry(e) {
+    e.preventDefault();
+    searchCountry();
 
   }
   return (
@@ -54,8 +65,8 @@ const Countries = () => {
       ) : (
         <section className="container mx-auto p-8">
             <div className="flex flex-col gap-4 md:flex-row md:items-center md:justify-between mb-8">
-                <form onSubmit={handleSearchCountry}autoComplete="off" className="max-w-4xl md:flex-1">
-                    <input type="text" name="search" id="search" placeholder="Search for a country" required className="py-3 px-4 text-gray-600 placeholder-gray-600 w-full shadow outline-none dark:text-gray-400 dark:placeholder-gray-400 dark:bg-gray-800 dark:focus:bg-gray-700 transition-all duration-200"/>
+                <form onSubmit={handleSearchCountry} autoComplete="off" className="max-w-4xl md:flex-1">
+                    <input type="text" name="search" value={searchText} onChange={(e) => setSearchText(e.target.value)} id="search" placeholder="Search for a country" required className="py-3 px-4 text-gray-600 placeholder-gray-600 w-full shadow outline-none dark:text-gray-400 dark:placeholder-gray-400 dark:bg-gray-800 dark:focus:bg-gray-700 transition-all duration-200"/>
                 </form>
                 <form >
                     <select name="filter-by-region" id="filter-by-region" className="w-52 py-3 px-4 outline-none shadow rounded text-gray-600 dark:text-gray-400 dark:bg-gray-800 dark:focus:bg-gray-700">
