970eb5afb614520bfe9581e7580374cb4f23d9bf
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-03-05 08:22:35 +0000

    function search

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -4,6 +4,7 @@ import Article from "./Article";
 
 const Countries = () => {
   const [countries, setCountries] = useState([]);
+  const [searchText, setSearchText] = useState("");
   const regions = [
     {
         name:"Europe"
@@ -37,6 +38,12 @@ const Countries = () => {
     };
     getCountries();
   }, []);
+
+  const handleSearchCountry = (e) => {
+    e.preventDefault()
+    SearchCountry()
+
+  }
   return (
     <>
       {!countries ? (
@@ -47,7 +54,7 @@ const Countries = () => {
       ) : (
         <section className="container mx-auto p-8">
             <div className="flex flex-col gap-4 md:flex-row md:items-center md:justify-between mb-8">
-                <form autoComplete="off" className="max-w-4xl md:flex-1">
+                <form onSubmit={handleSearchCountry}autoComplete="off" className="max-w-4xl md:flex-1">
                     <input type="text" name="search" id="search" placeholder="Search for a country" required className="py-3 px-4 text-gray-600 placeholder-gray-600 w-full shadow outline-none dark:text-gray-400 dark:placeholder-gray-400 dark:bg-gray-800 dark:focus:bg-gray-700 transition-all duration-200"/>
                 </form>
                 <form >
